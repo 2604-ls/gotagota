d9e8f9e89303b60ec4402c25fca18279ab60b373
leonardo <lsilvaramos802@gmail.com>
2026-04-17 15:59:36 -0500

    js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -172,7 +172,7 @@ function guardarCliente() {
 
                 cargarClientes();
                 limpiarFormulario();
- 
+
                 modoEdicion = false;
                 idClienteEditar = null;
 
